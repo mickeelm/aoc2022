6ad5e773806100b414b711cb70c2cc97f768ed5c
Micke Elm <elm.micke@gmail.com>
2022-12-02 00:10:34 +0100

    Day 01

---
+++ day01/src/Lib.hs
@@ -0,0 +1,32 @@
+module Lib (aoc) where
+
+import Data.List (sort)
+import System.Environment (lookupEnv)
+
+type Input = [Int]
+
+aoc :: IO ()
+aoc = do
+  input <- readInput
+  part <- lookupEnv "part"
+  print $ case part of
+    Just "part2" -> solve2 input
+    _ -> solve1 input
+
+solve1 :: [Int] -> Int
+solve1 = maximum
+
+solve2 :: [Int] -> Int
+solve2 = sum . take 3 . reverse . sort
+
+readInput :: IO Input
+readInput = parseInput <$> readFile "input.txt"
+
+parseInput :: String -> [Int]
+parseInput = map sum . chunked . lines
+
+chunked :: [String] -> [[Int]]
+chunked = foldl chunkAndParse [[]]
+
+chunkAndParse :: [[Int]] -> String -> [[Int]]
+chunkAndParse chunks str = if str == "" then chunks ++ [[]] else init chunks ++ [last chunks ++ [read str]]
